95d4a200c33f3130fd72255b8a5a780ad01af7dc
Vinicius da Silva Alves <112900025+OViniSA@users.noreply.github.com>
2025-01-30 16:52:30 -0300

    Um updatezin.

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -8,4 +8,8 @@ public interface DaoFactory {
     public static LivroDao createLivroDao() {
         return new LivroDaoJDBC(DB.getConnection());
     }
+
+    public static ClienteDao createClienteDao() {
+        return new ClienteDaoJDBC(DB.getConnection());
+    }
 }
