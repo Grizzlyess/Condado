006c00921852ec5b17ebaf02e3accf555240211e
Vinicius da Silva Alves <112900025+OViniSA@users.noreply.github.com>
2025-01-30 16:52:55 -0300

    Um updatezin.

---
+++ src/model/entities/Cliente.java
@@ -0,0 +1,36 @@
+package model.entities;
+
+public class Cliente {
+    private String id;
+    private String nome;
+    private String email;
+    private String contato;
+    
+    public String getId() {
+        return this.id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getNome() {
+        return this.nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getEmail() {
+        return this.email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getContato() {
+        return this.contato;
+    }
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+}
