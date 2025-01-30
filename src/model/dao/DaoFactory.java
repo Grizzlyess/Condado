43c6212b473da5bec4f0e450568e4c3d6c951f17
Vinicius da Silva Alves <112900025+OViniSA@users.noreply.github.com>
2025-01-30 15:33:25 -0300

    Ajeitando detalhes que ainda não existem.

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -2,10 +2,14 @@ package model.dao;
 
 import db.DB;
 import model.dao.impl.LivroDaoJDBC;
+import model.dao.impl.ClienteDaoJDBC;
 
 public interface DaoFactory {
     public static LivroDao createLivroDao() {
-        return new LivroDaoJDBC(DB.getConnection()) {
-        };
+        return new LivroDaoJDBC(DB.getConnection());
+    }
+
+    public static ClienteDao createClienteDao() {
+        return new ClienteDaoJDBC(DB.getConnection());
     }
 }
