592c27ed837342217cc66bd63ee3ab1f4b3e9200
Vinicius da Silva Alves <112900025+OViniSA@users.noreply.github.com>
2025-01-30 15:24:00 -0300

    Add files via upload

---
+++ src/model/dao/ClienteDao.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public interface ClienteDao {
+    void inserir(Cliente cliente);
+    void procurarPorId(Cliente cliente);
+    void procurarPorNome(Cliente cliente);
+    void procurarPorEmail(Cliente cliente);
+    void procurarPorContato(Cliente cliente);
+    void atualizar(Cliente cliente);
+    void deletarPorId(Cliente cliente);
+    List<Cliente> procurarTodos();
+}
